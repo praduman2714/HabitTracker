c2788e65ab1fe1d74502b371b8aa5b0e5bb51990
praduman2714 <pradu14jsr@gmail.com>
2023-04-05 23:59:38 +0530

    Added middleware for css, and provided basic styling to some pages

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,5 +1,6 @@
 module.exports.home = function(req, res){
     return res.render('home', {
         title : "Habit Tracker"
+        // userName : "Praduman"
     })
 }
--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.set('views','./views');
 
 app.use(expressLayout);
 // app.use(bodyParser.urlencoded({extended:false}));
-
+app.use(express.static('./assets'));
 
 
 app.use('/' , require('./routes/index')); // middleware for router
