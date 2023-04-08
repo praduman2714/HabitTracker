beb095c4c4b665c8cfe6de2c942f43cb7c5ec41b
praduman2714 <pradu14jsr@gmail.com>
2023-04-08 16:57:28 +0530

    Cloude Database

--- config/moongoose.js
+++ config/moongoose.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
-
-mongoose.connect(`mongodb://127.0.0.1/habitTracker`);
+const dbb = 'mongodb+srv://whiteWolf:praduman@cluster0.vji2hj2.mongodb.net/habitTracker?retryWrites=true&w=majority'
+mongoose.connect(dbb).then(()=>{
+    console.log('connection successful');
+}).catch((err) => console.log("no connection " + err));
 
 const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, "Error connecting to MongoDB"));
 
 
-db.once('open', async function(){
-    await console.log('Connected to Database :: MongoDB');
+db.once('open',  function(){
+     console.log('Connected to Database :: MongoDB');
 });
 
 
-module.exports = db;
+module.exports = db; 
